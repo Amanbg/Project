0dbd5674dd66bfa30df567ba370b8f6084ac8383
Amanbg <arvind@vujadelabs.com>
2016-10-05 12:23:40 +0530

    A simple Todo app using Mean stack

---
+++ public/core.js
@@ -0,0 +1,45 @@
+// create a module , controller, and define functions to handle todos.
+// Then we can apply to view.
+
+var scotchTodo = angular.module('scotchTodo', []);
+
+function mainController($scope, $http) {
+	$scope.formData = {};
+
+	// when landing on the page, get all todos and show them
+	$http.get('/api/todos')
+		.success(function(data) {
+			$scope.todos = data;
+			console.log(data);
+		})
+		.error(function(data) {
+			console.log('Error: '+ data);
+		});
+
+	// when submitting the form, send the text to the node API
+	$scope.createTodo = function() {
+		$http.post('/api/todos', $scope.formData)
+			.success(function(data) {
+				$scope.formData = {};
+				$scope.todos = data;
+				console.log(data);
+
+			})
+			.error(function(data) {
+				console.log('Error : ' + data);
+			});
+	};
+
+	//delete a todo after checking it..
+
+	$scope.deleteTodo = function(id) {
+		$http.delete('/api/todos/' + id)
+			.success(function(data) {
+				$scope.todos = data;
+				console.log(data);
+			})
+			.error(function(data) {
+				console.log('Error : ' + data);
+			});
+	};
+}
